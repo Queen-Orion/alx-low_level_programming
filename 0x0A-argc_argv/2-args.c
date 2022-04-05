fdd9ae4626dcb4c03b689af03cd8ac77244cbdfe
Queen-Orion <leobrer77@gmail.com>
2022-04-05 06:08:24 -0700

    democracy or democrazy, suit yourself

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,6 +11,10 @@ int main(int argc __attribute__((unused)), char *argv[])
 	printf("%s\n", argv[0]);
 	printf("%s\n", argv[1]);
 	printf("%s\n", argv[2]);
+	printf("%s\n", argv[3]);
+	printf("%s\n", argv[4]);
+	printf("%s\n", argv[5]);
+	printf("%s\n", argv[6]);
 	return (0);
 }
 
